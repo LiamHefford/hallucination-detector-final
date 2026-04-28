44e9c092f6e8f855c45aa68f5024109e553c548a
Liam Hefford <liamhefford7@gmail.com>
2026-04-28 17:56:52 +0100

    Update app_utils.py
    Added comments

--- application/modules/app_utils.py
+++ application/modules/app_utils.py
@@ -2,9 +2,11 @@ import os
 import re
 
 def clear_console():
+    # Clear the console based on the operating system, cls for Windows and clear for Linux
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def split_sentences(text: str):
+    # Split text into sentences using regex, splitting on punctuation (.!?)
     text = text.strip()
     if not text:
         return []
